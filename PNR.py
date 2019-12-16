b7d3b1e45f290f075289f1ffc8638c44258cfa04
mdtngr <mdtngr@gmail.com>
2019-12-16 13:31:00 +0530

    adds function changes

--- PNR.py
+++ PNR.py
@@ -1,22 +1,40 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
+import requests
 
 
-s=input("Pnr??: ")
+s=input("Pnr No??: ")
 driver = webdriver.Chrome()
 driver.get("https://www.trainspnrstatus.com/")
 
 from selenium.webdriver.common.by import By
 
-#s='2750486273'
+#s='4613209408'
 
 element = driver.find_element_by_id('fullname').send_keys(s)
 element = driver.find_element_by_id('idbtn').click()
 
+table_id = driver.find_element_by_class_name('table')
+rows = table_id.find_element_by_tag_name("tr") # get all of the rows in the table
+print(str(rows))
 
-# element=driver.find_element_by_id('indiGoFlightSchedule.Origin')
-# element.sendKeys("Ranchi")
-# element = driver.find_element_by_name("autocomplete-wrapper station-wrapper")
-# element = driver.find_element_by_name("indiGoFlightSchedule.Origin")
-# element.send_keys("some text")
+url = "https://www.trainspnrstatus.com/pnrformcheck.php"
+r= requests.get(url)
+def text(elt):
+    return elt.text_content().replace(u'\xa0', u' ')
+
+url = 'https://www.trainspnrstatus.com/pnrformcheck.php'
+r = requests.get(url)
+root = LH.fromstring(r.content)
+
+for table in root.xpath('//table[@id="td"]'):
+    header = [text(th) for th in table.xpath('//th')]        # 1
+    data = [[text(td) for td in tr.xpath('td')]  
+            for tr in table.xpath('//tr')]                   # 2
+    data = [row for row in data if len(row)==len(header)]    # 3 
+    data = pd.DataFrame(data, columns=header)                # 4
+    print(data)
+
+# driver.quit()
+     
---
+++ drivepush/searchfile.py
@@ -0,0 +1,44 @@
+from __future__ import print_function
+import httplib2
+import os, io
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+from apiclient.http import MediaFileUpload, MediaIoBaseDownload
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+import auth
+# If modifying these scopes, delete your previously saved credentials
+# at ~/.credentials/drive-python-quickstart.json
+SCOPES = 'https://www.googleapis.com/auth/drive'
+CLIENT_SECRET_FILE = '/home/mudit/Desktop/bazinga_ML_winter19/drivepush/client_secret.json'
+APPLICATION_NAME = 'Drive API Python Quickstart'
+authInst = auth.auth(SCOPES,CLIENT_SECRET_FILE,APPLICATION_NAME)
+credentials = authInst.getCredentials()
+
+http = credentials.authorize(httplib2.Http())
+drive_service = discovery.build('drive', 'v3', http=http)
+ip = input("Enter name of file to search: ")
+
+def searchFol(val):
+    page_token = None
+    while True:
+        response = drive_service.files().list(q="mimeType='image/jpeg'",
+                                            spaces='drive',
+                                            fields='nextPageToken, files(id, name)',
+                                            pageToken=page_token).execute()
+        for file in response.get('files', []):
+            # Process change
+            if(file.get('name')== val):
+                print('Found file: ', file.get('name'))
+        
+        page_token = response.get('nextPageToken', None)
+        if page_token is None:
+            break
+
+searchFol(ip)
